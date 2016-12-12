555ec7d99bb7c02e2c1a66e0e4decb93e39fdb3c
olajideabiodun <olajide0322@saskpolytech.ca>
2016-12-12 14:37:12 -0600

    Add files via upload

---
+++ AbbeyProject1.java
@@ -0,0 +1,16 @@
+
+package abbey.project1;
+
+/**
+ * @author olajide0322
+ */
+public class AbbeyProject1 {
+
+    
+    public static void main(String[] args) {
+        // Say Hello
+        String name = "Abbey";
+        System.out.println("Hello, World from  "+ name);
+    }
+    
+}
